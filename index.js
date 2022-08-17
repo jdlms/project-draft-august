f1f9f42395f202e7f0e515b4f89f480dc1034459
jdlms <97018563+jdlms@users.noreply.github.com>
2022-08-17 17:58:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 let boat;
 
-x = 30;
-y = 20;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   speed = 3;
+  x = windowWidth / 2;
+  y = windowHeight / 2;
 }
 
-function drawArrow(base, vec, myColor) {
-  push();
-  stroke(myColor);
-  strokeWeight(3);
-  fill(myColor);
-  translate(base.x, base.y);
-  line(0, 0, vec.x, vec.y);
-  rotate(vec.heading());
-  let arrowSize = 7;
-  translate(vec.mag() - arrowSize, 0);
-  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-  pop();
-}
+// function drawArrow(base, vec, myColor) {
+//   push();
+//   stroke(myColor);
+//   strokeWeight(3);
+//   fill(myColor);
+//   translate(base.x, base.y);
+//   line(0, 0, vec.x, vec.y);
+//   rotate(vec.heading());
+//   let arrowSize = 7;
+//   translate(vec.mag() - arrowSize, 0);
+//   triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+//   pop();
+// }
 
 function player() {
   rect(x, y, 55, 55, 20);
   fill("rgb(135, 62, 35)");
   stroke(255, 204, 0);
 
-  const vx = 
-  line(x + 55 / 2, y + 55 / 2, mouseX, mouseY);
+  const vx = line(x + 55 / 2, y + 55 / 2, mouseX, mouseY);
 
   //
 
